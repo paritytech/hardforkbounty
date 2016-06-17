824f91f6b1d68aafcf9af1d16e66460b1a6838d5
Gav Wood <gavin@ethcore.io>
2016-06-17 21:15:59 +0200

    More docs.

--- hardforkbounty.sol
+++ hardforkbounty.sol
@@ -3,12 +3,14 @@
 /// Licenced under Apache 2.
 
 /// A bounty program for ensuring the DAO hard fork does through.
-/// Everyone can put down a refundable deposit; miners can collect half of the
-/// remaining deposit, once per block, only when the DAO has had all funds
-/// returned and when the code has been changed (ideally we'll propose that
-/// change, too). If the bounty isn't paid (hard fork doesn't go through) then
-/// deposits can be claimed once the block hits 1.9M.
+/// Everyone can put down a refundable deposit; miners can collect some fixed
+/// proportion of the remaining deposit, once per block, only when the DAO has
+/// had all funds returned and when the code has been changed (ideally we'll 
+/// propose that change, too). If the bounty isn't paid (hard fork doesn't go
+/// through) then deposits can be claimed once the block hits 1.9M.
 contract HardForkBounty {
+    // Get the code at a particular address. Code provided by @chriseth.
+    // Review carefully.
     function at(address _addr) returns (bytes o_code) {
         assembly {
             // retrieve the size of the code, this needs assembly
@@ -34,12 +36,17 @@ contract HardForkBounty {
         if (msg.value > 0)
             balances[msg.sender] += msg.value;
         else if (
+            // Ensure that TheDAO's code has been changed...
+            // TODO: ...to a known good contract e.g. WithdrawDAO.
             sha3(at(0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413)) != 0x7278d050619a624f84f51987149ddb439cdaadfba5966f7cfaea7ad44340a4ba &&
-            // TODO: replace with == <actual good code hash>
+            // If it's the first reward claim, ensure that the stolen Ether has been put back in TheDAO.
             (address(0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413).balance > 11000000 ether || lastPayout > 0) &&
+            // Ensure that this only happens once per block.
             now > lastPayout
         ) {
-            block.coinbase.send(this.balance / 2);
+            // Send back some proportion of the funds. A tenth for now.
+            block.coinbase.send(this.balance / 10);
+            // Record payout to avoid multiple claims on the same block.
             lastPayout = now;
         }
     }
@@ -47,6 +54,7 @@ contract HardForkBounty {
     /// Deposit function. Deposits ether on behalf of a third party.
     /// This is useful for people who only have exchange accounts.
     function deposit(address _who) {
+        // Add the deposited ether to the account balance.
         balances[_who] += msg.value;
     }
     
@@ -55,10 +63,13 @@ contract HardForkBounty {
     /// it can be used. We assume that the hard fork has happened and the bounty
     /// paid by then, so there's no need for additional checks.
     function withdraw() {
+        // Only if the block number is well-after hard-fork time:
         if (block.number > 1900000) {
+            // Figure out how much we should be repaying (b).
             var b = balances[msg.sender];
             // Learn the lesson! Set to zero *and then* call send!
             balances[msg.sender] = 0;
+            // Do the refund.
             msg.sender.send(b);
         }
     }
